5c5c5f243fc9a0c74bd52a6a8c3d148da02ad249
Hamza Hanif <hamzahanif1024@gmail.com>
2024-09-05 21:38:43 +0500

    hero section starts

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function HeroSection() {
   return (
     <>
-        <div className="bg-[#070707]">
+        <div className="bg-[#070707] text-white text-8xl font-bold">
           Hero Section
         </div>
     </>
